289786e8c44072c9f534873bbcc131e678c10869
saulwade <saulwade29@gmail.com>
2023-05-17 08:16:56 -0600

    fixed some bugs

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -2,10 +2,6 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
