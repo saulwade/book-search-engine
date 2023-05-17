00b086fac1e186779d6795c41cbeab6cba411161
saulwade <saulwade29@gmail.com>
2023-05-17 07:50:27 -0600

    added more code

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Define Apollo Server
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
+async function startApolloServer(typeDefs, resolvers) {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  
+  await server.start();
 
-server.applyMiddleware({ app });
+  server.applyMiddleware({ app });
 
-app.listen(PORT, () => {
-  console.log(`🌍 Now listening on localhost:${PORT}`);
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-});
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+}
+
+startApolloServer(typeDefs, resolvers);
